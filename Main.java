8e932c08fb7579c1119116b2eeb25ac2ce7d337d
gogosauce <hgo.roger@wanadoo.fr>
2017-01-29 22:05:50 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
-package laby;
-
 public class Main {
 	public static void main(String[] args) {
 		Grid maze= new Grid();
-		maze.displayGrid();	  
+		maze.delimiteEdge();
+		maze.openedWall();
+		int val = 0;		
+		for (int i=0; i<maze.getHeight(); i++){
+			for (int j=0; j<maze.getWidth(); j++){
+				val++;
+				maze.getCase(i, j).setVal(val);
+			}
+		}
+		
+		Frame fram = new Frame(maze);
 		
 	}
 }
